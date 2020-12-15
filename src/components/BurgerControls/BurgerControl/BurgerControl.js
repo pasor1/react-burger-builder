3a03de902c9247e335b9f801b590c919c12e8646
pasor1 <pasor1@gmail.com>
2020-12-15 21:12:30 +0100

    disabable minus button if no ingredient

--- src/components/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/BurgerControls/BurgerControl/BurgerControl.js
@@ -4,7 +4,10 @@ const BurgerControl = (props) => {
   return (
     <div>
       {props.label}
-
+      {props.disabled
+        ? <button disabled type="button" onClick={props.onClickMinus} className="py-1 px-3 m-2 bg-gray-300 text-white font-semibold rounded-lg shadow-md focus:outline-none cursor-default">-</button>
+        : <button type="button" onClick={props.onClickMinus} className="py-1 px-3 m-2 bg-red-700 text-white font-semibold rounded-lg shadow-md focus:outline-none hover:bg-red-600 hover:scale-95 transform transition duration-50 ease-in-out">-</button>
+      }
       <button type="button" onClick={props.onClickPlus} className="py-1 px-3 m-2 bg-green-800 text-white font-semibold rounded-lg shadow-md focus:outline-none hover:bg-green-700 hover:scale-95 transform transition duration-50 ease-in-out">+</button>
     </div>
   )
