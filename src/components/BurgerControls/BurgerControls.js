0f3dc7f6e149e1780290a34f53d6a8b6fc3851f3
pasor1 <pasor1@gmail.com>
2020-12-15 21:19:10 +0100

    improve controls layout

--- src/components/BurgerControls/BurgerControls.js
+++ src/components/BurgerControls/BurgerControls.js
@@ -4,17 +4,19 @@ import { INGREDIENTS_LIST } from '../../containers/BurgerBuilder/BurgerBuilder'
 
 const BurgerControls = (props) => {
   return (
-    <div className="max-w-md mx-auto mt-4 bg-gray-100 text-right">
-      {INGREDIENTS_LIST.map((item) => (
-        <BurgerControl
-          key={item.ingredient}
-          label={item.label}
-          disabled={!props.ingredients.includes(item.ingredient)}
-          onClickPlus={() => props.onClickPlus(item.ingredient)}
-          onClickMinus={() => props.onClickMinus(item.ingredient)} />
-      ))}
-      <hr />
-      <p className="text-xl">TOTAL {props.total}</p>
+    <div className="bg-gray-100 p-4">
+      <div className="max-w-md mx-auto mt-4 text-right">
+        {INGREDIENTS_LIST.map((item) => (
+          <BurgerControl
+            key={item.ingredient}
+            label={item.label}
+            disabled={!props.ingredients.includes(item.ingredient)}
+            onClickPlus={() => props.onClickPlus(item.ingredient)}
+            onClickMinus={() => props.onClickMinus(item.ingredient)} />
+        ))}
+        <hr className="my-3" />
+        <p className="text-xl">TOTAL {props.total}</p>
+      </div>
     </div>
   )
 }
